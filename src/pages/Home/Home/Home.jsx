e569cd92a29686e31766a8c75fa8a2f6c5433b43
emonh0que <you@example.com>
2023-06-10 12:59:25 +0600

    popular courses section is done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ShowSlider from '../../ShowSlider/ShowSlider';
+import PopularCourses from '../../PopularCourses/PopularCourses';
 
 
 const Home = () => {
     return (
         <div>
             <ShowSlider></ShowSlider>
+            <PopularCourses></PopularCourses>
         </div>
     );
 };
