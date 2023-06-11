6266300a6e9da580ca7bf29b5bde5260263e5836
emonh0que <you@example.com>
2023-06-11 14:26:45 +0600

    made classes page dynamic

--- src/components/Classes/Classes.jsx
+++ src/components/Classes/Classes.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import usePopularClasses from "../../hooks/usePopularClasses";
+import SingleClasses from "../SingleClasses/SingleClasses";
 
 const Classes = () => {
+  const [popularClasses] = usePopularClasses();
   return (
     <div>
       <h2 className="text-[#192335] font-bold text-[25px] md:text-[42px] text-center my-10">
@@ -21,32 +24,10 @@ const Classes = () => {
           </thead>
 
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <td>
-                <p>English For Beginners</p>
-              </td>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="rounded-full w-12 h-12">
-                      <img
-                        src="https://bambino.bold-themes-cdn.com/demo-02/wp-content/uploads/sites/3/2019/02/team_01.jpg"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td>Rebecca Hill</td>
-              <td>20</td>
-              <td>$ 300</td>
-              <td>
-                <button className="bg-gradient-to-r from-[#2f57ef] to-[#B260EC] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Enroll Now
-                </button>
-              </td>
-            </tr>
+           
+            {
+              popularClasses.map(rowData => <SingleClasses key={rowData.name} rowData={rowData}></SingleClasses>)
+            }
           </tbody>
         </table>
       </div>
---
+++ src/components/SingleClasses/SingleClasses.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SingleClasses = ({rowData}) => {
+    const {name, instructor_name, image, available_seat, price} = rowData;
+    return (
+        <tr>
+              <td>
+                <p>{name}</p>
+              </td>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="rounded-full w-12 h-12">
+                      <img
+                        src={image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td>{instructor_name}</td>
+              <td>{available_seat}</td>
+              <td>$ {price}</td>
+              <td>
+                <button className="bg-gradient-to-r from-[#2f57ef] to-[#B260EC] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Enroll Now
+                </button>
+              </td>
+            </tr>
+    );
+};
+
+export default SingleClasses;
