1cbb936ca0b71e4b53da927c069d1f5e5aef8437
emonh0que <you@example.com>
2023-06-14 13:04:07 +0600

    made the instructor page secure

--- src/hooks/useMyClasses.jsx
+++ src/hooks/useMyClasses.jsx
@@ -4,15 +4,21 @@ import { AuthContext } from '../providers/AuthProviders';
 
 const useMyClasses = () => {
     const {user} = useContext(AuthContext);
-
+    const token = localStorage.getItem('access-token');
     const {data: myClasses = [], isLoading: loading, refetch} = useQuery({
         queryKey: ['my_classes'],
         queryFn: async() => {
-            const res = await fetch(`http://localhost:5000/classes/${user?.email}`);
+            const res = await fetch(`http://localhost:5000/classes/${user?.email}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
+            });
+            
             return res.json();
         }
         
     })
+    console.log(myClasses);
     return [myClasses, loading, refetch];
 };
 
